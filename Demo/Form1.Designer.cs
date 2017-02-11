a3066df62c7dc4591ff023de991da418dcf621b5
Rodolfo Ortiz <rodolfo.ortiz@definityfirst.com>
2017-02-11 13:49:54 -0600

    adding new code

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -32,13 +32,16 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(11, 12);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 157);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(493, 38);
+            this.button1.Size = new System.Drawing.Size(1074, 72);
             this.button1.TabIndex = 0;
             this.button1.Text = "Expressions with lambda expressions";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +49,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(11, 56);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 235);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(493, 57);
+            this.button2.Size = new System.Drawing.Size(1074, 59);
             this.button2.TabIndex = 1;
             this.button2.Text = "Expressions with the expressions API";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,9 +60,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(11, 118);
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 300);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(493, 57);
+            this.button3.Size = new System.Drawing.Size(1074, 59);
             this.button3.TabIndex = 1;
             this.button3.Text = "Expressions with the expressions API (alternative)";
             this.button3.UseVisualStyleBackColor = true;
@@ -66,19 +71,44 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(11, 180);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 365);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(493, 57);
+            this.button4.Size = new System.Drawing.Size(1074, 65);
             this.button4.TabIndex = 2;
-            this.button4.Text = "Expressions with the expressions API (alternative)";
+            this.button4.Text = "Lambda expression with expression tree";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(16, 17);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(1070, 58);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Reflection";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(16, 81);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(1070, 70);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "CodeDOM";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 479);
+            this.ClientSize = new System.Drawing.Size(1106, 525);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -95,6 +125,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Demo
@@ -24,16 +28,16 @@ namespace Demo
 
         private void button2_Click(object sender, EventArgs e)
         {
+
             var x = Expression.Parameter(typeof(int));
             var y = Expression.Parameter(typeof(int));
 
-            var total= (Expression.Lambda(
-                Expression.Add(x, y), x, y)
+            var total = (Expression.Lambda(Expression.Add(x, y), x, y)
                 .Compile() as Func<int, int, int>)(2, 3);
 
             MessageBox.Show(total.ToString());
         }
-        
+
         private void button3_Click(object sender, EventArgs e)
         {
             Expression sumExpr = Expression.Add(
@@ -55,13 +59,14 @@ namespace Demo
             };
 
             // var results = people.Where(x => x.Name.Contains("a"));
+            var inputText = "a";
 
             var argument = Expression.Parameter(typeof(Person));
             var valueProperty = Expression.Property(argument, "Name");
             var containsCall = Expression.Call(valueProperty,
                 typeof(string).GetMethod(
                     "Contains", new Type[] {typeof(string)}),
-                Expression.Constant("a", typeof(string)));
+                Expression.Constant(inputText, typeof(string)));
             var wherePredicate = Expression.Lambda<Func<Person, bool>>(
                 containsCall, argument);
             var whereCall = Expression.Call(typeof(Queryable), "Where",
@@ -73,8 +78,63 @@ namespace Demo
             MessageBox.Show(string.Join(", ",
                 expressionResults.ToList().Select(x => x.Name)));
         }
-    }
 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CodeNamespace prgNamespace = HelloWorldCodeDOM.BuildProgram();
+            var compilerOptions = new CodeGeneratorOptions()
+            {
+                IndentString = " ",
+                BracingStyle = "C",
+                BlankLinesBetweenMembers = false
+            };
+            var codeText = new StringBuilder();
+            using (var codeWriter = new StringWriter(codeText))
+            {
+                CodeDomProvider.CreateProvider("vb")
+                .GenerateCodeFromNamespace(
+                prgNamespace, codeWriter, compilerOptions);
+            }
+            var script = codeText.ToString();
+            MessageBox.Show(script);
+        }
+
+        partial class HelloWorldCodeDOM
+        {
+            public static CodeNamespace BuildProgram()
+            {
+                var ns = new CodeNamespace("MetaWorld");
+                var systemImport = new CodeNamespaceImport("System");
+                ns.Imports.Add(systemImport);
+                var programClass = new CodeTypeDeclaration("Program");
+                ns.Types.Add(programClass);
+                var methodMain = new CodeMemberMethod
+                {
+                    Attributes = MemberAttributes.Static,
+                    Name = "Main"
+                };
+                methodMain.Statements.Add(new CodeMethodInvokeExpression(new CodeSnippetExpression("Console"), "WriteLine",
+                    new CodePrimitiveExpression("Hello, world!")));
+                programClass.Members.Add(methodMain);
+                return ns;
+            }
+        }
+
+        private static int a = 10, b = 20;
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Type t = typeof(Form1);
+            string varName = "a";
+            FieldInfo fieldInfo = t.GetField(varName, BindingFlags.NonPublic | BindingFlags.Static);
+            if (fieldInfo != null)
+            {
+                int newInt = -10;
+                fieldInfo.SetValue(null, newInt);
+                MessageBox.Show("a * b = " + (a * b));
+            }
+        }
+    }
+    
     public sealed class Person
     {
         public string Name { get; set; }
